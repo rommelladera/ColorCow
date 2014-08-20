f8aeb831c2e56be488b7c7374230750178ea1c93
rommel.ladera@rhythmonfire.com <0711Dergib##>
2014-08-20 16:44:24 -0500

    fhfgh

--- canvas.js
+++ canvas.js
@@ -5,11 +5,16 @@ canvas.context = canvas.getContext("2d");
 canvas.orientation = null;
 
 canvas.onLoad = function () {
-    canvas.width = viewport.width - 1; //480
-    canvas.height = viewport.height - 1; //360;
-    canvas.orientation = viewport.orientation;
-
     canvas.style.background = "blue";
+    canvas.width = 480
+    canvas.height = 360;
+    canvas.onRefresh();
+
+    var imageObj = new Image();
+    imageObj.src = 'images/tree.jpg';
+    imageObj.onload = function () {
+        canvas.context.drawImage(imageObj, 10, 10);
+    };
 
     //canvas.context.fillStyle = "blue";
     //canvas.context.fillRect(0, 0, canvas.width, canvas.height);
@@ -19,5 +24,26 @@ canvas.onLoad = function () {
 };
 
 canvas.onOrientationChange = function (orientation) {
-    canvas.onLoad();
+    //canvas.onRefresh();
+    canvas.orientation = viewport.orientation;
+
+    canvas.context.translate(canvas.width / 2, canvas.height / 2);
+    if (orientation == "Landscape") {
+        canvas.context.rotate(90 * Math.PI / 180);
+    } else {
+        canvas.context.rotate(-90 * Math.PI / 180);
+    }
+    canvas.context.translate(-canvas.width / 2, -canvas.height / 2);
 };
+
+canvas.onRefresh = function () {
+    canvas.orientation = viewport.orientation;
+
+    if (canvas.orientation == "Landscape") {
+        canvas.style.width = "100px";
+        canvas.style.height = "75px";
+    } else {
+        canvas.style.width = "75px";
+        canvas.style.height = "100px";
+    }
+}
