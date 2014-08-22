81322576f86a1f280d93970e838cbb52d8301be8
rommel.ladera@rhythmonfire.com <0711Dergib##>
2014-08-21 20:09:11 -0500

    dfgdgds

---
+++ cowgirl.js
@@ -0,0 +1,15 @@
+﻿var cowgirl = new Image();
+cowgirl.x = 10;
+cowgirl.y = 10;
+cowgirl.sizeWidth = 50;
+cowgirl.sizeHeight = 50;
+
+cowgirl.onLoad = function (callback) {
+    debugger;
+    cowgirl.src = 'images/cowgirl.png';
+    cowgirl.onload = callback;
+}
+
+cowgirl.onDraw = function () {
+    canvas.context.drawImage(cowgirl, cowgirl.x, cowgirl.y, cowgirl.sizeWidth, cowgirl.sizeHeight);
+}
