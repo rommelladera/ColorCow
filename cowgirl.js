ea39d79a4fd2a511b16bef715eabbee2c4b98e32
rommel.ladera@rhythmonfire.com <0711Dergib##>
2014-09-07 15:40:35 -0500

    sfsdf

--- cowgirl.js
+++ cowgirl.js
@@ -10,16 +10,16 @@ cowgirl.directionY = 1;
 
 
 
-cowgirl.horseSound = new Audio("horseRun.mp3");
+cowgirl.horseSound = new Audio("app/www/horseRun.mp3");
 cowgirl.horseSound.play();
 
-alert(window.location.href);
-cowgirl.horseSound = new Audio(window.location.href.replace("index.html", "horseRun.mp3"));
-cowgirl.horseSound.play();
+//alert(window.location.href);
+//cowgirl.horseSound = new Audio(window.location.href.replace("index.html", "horseRun.mp3"));
+//cowgirl.horseSound.play();
 
-alert(window.location.pathname);
-cowgirl.horseSound = new Audio(window.location.pathname.replace("index.html", "horseRun.mp3"));
-cowgirl.horseSound.play();
+//alert(window.location.pathname);
+//cowgirl.horseSound = new Audio(window.location.pathname.replace("index.html", "horseRun.mp3"));
+//cowgirl.horseSound.play();
 
 
 
